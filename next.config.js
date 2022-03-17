f1f5504e7c09ab5dbb590b58edc891050db56828
graffitiwriter <info@spannerspencer.com>
2022-03-17 10:55:01 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,13 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 module.exports = withNextra()
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/posts/hello-vercel',
+        destination: 'https://vercel.com',
+        permanent: false,
+      },
+    ]
+  },
+}
